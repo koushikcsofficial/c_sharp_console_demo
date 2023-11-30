4a9b045f97927c14ce258a900fd8a4c78b4b10ba
KOUSHIK SAHA <koushik.official1999@gmail.com>
2023-12-01 01:23:52 +0530

    sample message

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Linq;
+
+List<string> dbRoles = new(){"Admin", "User"};
+List<string> roles = new(){"SuperAdmin"};
+
+bool result = dbRoles.Any(role=> roles.Contains(role));
+
+Console.WriteLine(result);
